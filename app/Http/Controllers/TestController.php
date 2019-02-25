78d32a9905d34b71e9e5295713d554c4b80a2691
Robert <robcampbell@ucmo.edu>
2019-02-25 11:20:27 -0600

    Comment out everything inside this class

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -6,17 +6,17 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    protected $mytestModel;
-
-    public function __construct(Request $request) {
-      //parent::__construct($request);
-      $this->mytestModel = new \App\Mytest();
-    }
-
-    public function index() {
-      $data = $this->mytestModel->Call();
-
-      return view('mytest.index')->with('data', $data);
-
-    }
+    // protected $mytestModel;
+    //
+    // public function __construct(Request $request) {
+    //   //parent::__construct($request);
+    //   $this->mytestModel = new \App\Mytest();
+    // }
+    //
+    // public function index() {
+    //   $data = $this->mytestModel->Call();
+    //
+    //   return view('mytest.index')->with('data', $data);
+    //
+    // }
 }
