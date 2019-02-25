351b243bc1e6a06bdfe481e08a757d1c2beed3a5
Robert <robcampbell@ucmo.edu>
2019-02-25 11:04:43 -0600

    Display information on second historical vampire

--- resources/views/mytest/index.blade.php
+++ resources/views/mytest/index.blade.php
@@ -4,12 +4,26 @@
 
 <h1>We Love GO!!!</h1>
 
-<h2>Dracula</h2>
+<h2>Famous Real Vampires</h2>
+
+<br>
+
+<h3>Dracula</h3>
 <?php
   foreach($data[0] as $result) {
     echo $result . "<br>";
   }
 
- ?>
+?>
+
+<br><br>
+
+<h3>The Blood Countess</h3>
+<?php
+  foreach($data[1] as $result) {
+   echo $result . "<br>";
+  }
+
+?>
 
 @endsection
